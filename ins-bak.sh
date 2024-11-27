900cee683dfa29be4110c79b319b8f7e335d9cba
lswmxl <128008687+lswmxl@users.noreply.github.com>
2024-11-27 23:18:54 +0800

    Update ins-bak.sh

--- ins-bak.sh
+++ ins-bak.sh
@@ -151,8 +151,7 @@ install_x-ui() {
     rm x-ui-linux-${arch}.tar.gz -f
     cd x-ui
     #rm xray-linux-${arch} -f
-    wget --no-check-certificate -O https://amd11.870710.xyz/xui/x-ui-linux-${arch}1
-            if [[ $? -ne 0 ]]; then
+    wget --no-check-certificate -O https://amd11.870710.xyz/xui/x-ui-linux-${arch}.1
     chmod +x x-ui bin/xray-linux-${arch}
     cp -f x-ui.service /etc/systemd/system/
     wget --no-check-certificate -O /usr/bin/x-ui https://5th.pp.ua/https://raw.githubusercontent.com/lswmxl/sbui/refs/heads/main/x-ui_CN.sh
