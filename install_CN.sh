f58691a8b7c91fbbe906cb186183785260bcf77f
lswmxl <lswm@140504.xyz>
2024-11-28 16:59:57 +0800

    updata

--- install_CN.sh
+++ install_CN.sh
@@ -147,7 +147,7 @@ install_x-ui() {
         rm /usr/local/x-ui/ -rf
     fi
 
-    tar zxvf x-ui-linux-${arch}.tar.gz
+    tar xvf x-ui-linux-${arch}.tar.gz
     rm x-ui-linux-${arch}.tar.gz -f
     cd x-ui
     chmod +x x-ui bin/xray-linux-${arch}
